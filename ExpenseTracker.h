cb2e7b3ca3ff52ce756a2e82a7e9762edf709331
Bradley F Botros <143047175+BradleyB30@users.noreply.github.com>
2024-03-17 16:58:58 -0700

    Update ExpenseTracker.h

--- ExpenseTracker.h
+++ ExpenseTracker.h
@@ -1,1 +1,20 @@
+#ifndef EXPENSE_TRACKER_H
+#define EXPENSE_TRACKER_H
+
+#include "expense.h"
+#include <vector>
+
+class ExpenseTracker {
+public:
+    void addExpense(const std::string& category, double amount, const std::string& description);
+    void displayExpenses() const;
+
+private:
+    std::vector<Expense> expenses_;
+};
+
+#endif
+
+
+
 
