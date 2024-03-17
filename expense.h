1adcf79daeddd2394e086ef87141ffac07223f07
Bradley F Botros <143047175+BradleyB30@users.noreply.github.com>
2024-03-17 16:58:27 -0700

    Update expense.h

--- expense.h
+++ expense.h
@@ -1,1 +1,21 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
+
+#include <string>
+#include <iostream>
+
+class Expense {
+    public:
+        Expense(const std::string& category, double amount, const std::string& description);
+        std::string getCategory() const;
+        double getAmount() const;
+        std::string getDescription() const;
+        
+    private:
+        std::string category_;
+        double amount_;
+        std::string description_;
+};
+
+#endif
 
