9667c13f914fcf90e8300039c064204fc6c8916b
Bradley F Botros <143047175+BradleyB30@users.noreply.github.com>
2024-03-17 16:57:05 -0700

    Update expense.cc

--- expense.cc
+++ expense.cc
@@ -1,1 +1,20 @@
+#include "expense.h"
+
+Expense::Expense(const std::string& category, double amount, const std::string& description)
+    : category_(category), amount_(amount), description_(description) {}
+
+std::string Expense::getCategory() const {
+    return category_;
+}
+
+double Expense::getAmount() const {
+    return amount_;
+}
+
+std::string Expense::getDescription() const {
+    return description_;
+}
+
+
+
 
